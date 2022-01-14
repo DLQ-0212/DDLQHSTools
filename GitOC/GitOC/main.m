c54d8601fcc7e9d3dfdf6ee2d9f05e3ea94ec88e
DLQ-0212 <dulq@wisecrm.com>
2022-01-14 13:35:25 +0800

    初始化项目

---
+++ GitOC/GitOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitOC
+//
+//  Created by myMini on 2022/1/14.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
